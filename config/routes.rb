e9dea65e76b0ba432cfa5c24cede3a82bd69d771
Melkin Mosquera <melanmosta@gmail.com>
2021-03-23 10:09:34 -0500

    displaying messages and login implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'chatroom#index'
   get 'login', to: 'sessions#new'
-  resources :users
-  resources :messages
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  post 'message', to: 'messages#create'
 end
